0ee7e5d43fab57858413a6d46c53019390e16420
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2019-11-06 19:30:22 +0200

    Improved parser test

--- src/test/java/com/alekseyzhelo/evilislands/mobplugin/script/EIScriptParserTest.java
+++ src/test/java/com/alekseyzhelo/evilislands/mobplugin/script/EIScriptParserTest.java
@@ -1,19 +1,19 @@
 package com.alekseyzhelo.evilislands.mobplugin.script;
 
 import com.google.common.base.Stopwatch;
-import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
-import com.intellij.psi.PsiFile;
-import com.intellij.testFramework.LightVirtualFile;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.testFramework.ParsingTestCase;
+import com.intellij.testFramework.TestDataFile;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.Assert.*;
 
 public class EIScriptParserTest extends ParsingTestCase {
 
@@ -28,25 +28,25 @@ public class EIScriptParserTest extends ParsingTestCase {
     }
 
     public void testIngosTownOkrestTime() {
-        final int reps = 20;
-        int ms = 0;
-        for (int i = 0; i < reps; i++) {
-            final Stopwatch stopwatch = Stopwatch.createStarted();
-            doTestFast();
-            ms += stopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
-        }
-        LOG.info("Time of execution: {} ms", ms / reps);
-    }
-
-    private void doTestFast() {
         String name = getTestName();
         try {
             String text = loadFile(name + "." + myFileExt);
-            myFile = createPsiFile(name, text);
-            ensureParsed(myFile);
+            final int reps = 20;
+            int ms = 0;
+            for (int i = 0; i < reps; i++) {
+                final Stopwatch stopwatch = Stopwatch.createStarted();
+                doParse(name, text);
+                ms += stopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
+            }
+            LOG.info("Time of execution: {} ms", ms / reps);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void doParse(String name, String text) {
+        myFile = createPsiFile(name, text);
+        ensureParsed(myFile);
     }
 
     @Override
@@ -62,5 +62,10 @@ public class EIScriptParserTest extends ParsingTestCase {
     @Override
     protected boolean includeRanges() {
         return true;
+    }
+
+    @Override
+    protected String loadFile(@NotNull @TestDataFile String name) throws IOException {
+        return FileUtil.loadFile(new File(myFullDataPath, name), "windows-1251", true).trim();
     }
 }
