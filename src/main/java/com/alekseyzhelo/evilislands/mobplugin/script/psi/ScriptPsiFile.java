4e58415a421b28a5b379807555ed06ee11e8a4ea
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2019-11-16 13:33:02 +0200

    Replaced cache provider classes with lambdas

--- src/main/java/com/alekseyzhelo/evilislands/mobplugin/script/psi/ScriptPsiFile.java
+++ src/main/java/com/alekseyzhelo/evilislands/mobplugin/script/psi/ScriptPsiFile.java
@@ -52,7 +52,10 @@ public class ScriptPsiFile extends PsiFileBase {
     // TODO: return Maps instead of lists?
     @NotNull
     public List<EIGlobalVar> findGlobalVars() {
-        return CachedValuesManager.getCachedValue(this, new CachedGlobalVarsProvider());
+        final EIGlobalVars globalVars = PsiTreeUtil.getChildOfType(ScriptPsiFile.this, EIGlobalVars.class);
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(PsiTreeUtil.getChildrenOfTypeAsList(globalVars, EIGlobalVar.class),
+                        globalVars != null ? globalVars : this));
     }
 
     @Nullable
@@ -62,7 +65,10 @@ public class ScriptPsiFile extends PsiFileBase {
 
     @NotNull
     public List<EIScriptDeclaration> findScriptDeclarations() {
-        return CachedValuesManager.getCachedValue(this, new CachedScriptDeclarationsProvider());
+        final EIDeclarations declarations = PsiTreeUtil.getChildOfType(ScriptPsiFile.this, EIDeclarations.class);
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(PsiTreeUtil.getChildrenOfTypeAsList(declarations, EIScriptDeclaration.class),
+                        declarations != null ? declarations : this));
     }
 
     @Nullable
@@ -73,7 +79,10 @@ public class ScriptPsiFile extends PsiFileBase {
 
     @NotNull
     public List<EIScriptImplementation> findScriptImplementations() {
-        return CachedValuesManager.getCachedValue(this, new CachedScriptImplementationsProvider());
+        final EIScripts implementations = PsiTreeUtil.getChildOfType(this, EIScripts.class);
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(PsiTreeUtil.getChildrenOfTypeAsList(implementations, EIScriptImplementation.class),
+                        implementations != null ? implementations : this));
     }
 
     @Nullable
@@ -83,22 +92,30 @@ public class ScriptPsiFile extends PsiFileBase {
 
     @NotNull
     public Map<EITypeToken, List<LookupElement>> getGlobalVarLookupElements() {
-        return CachedValuesManager.getCachedValue(this, new CachedGlobalVarLookupElementsProvider());
+        EIGlobalVars globalVars = findChildByClass(EIGlobalVars.class);
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(getGlobalVarLookupElementsInner(),
+                        globalVars != null ? globalVars : this));
     }
 
     @NotNull
     public List<LookupElement> getScriptLookupElements() {
-        return CachedValuesManager.getCachedValue(this, new CachedScriptLookupElementsProvider());
+        final EIDeclarations declarations = PsiTreeUtil.getChildOfType(this, EIDeclarations.class);
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(getScriptLookupElementsInner(),
+                        declarations != null ? declarations : this));
     }
 
     @NotNull
     public Map<String, EIGSVar> findGSVars() {
-        return CachedValuesManager.getCachedValue(this, new CachedGSVarsProvider());
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(findGSVarsInner(), this));
     }
 
     @NotNull
     public Map<Integer, EIArea> findAreas() {
-        return CachedValuesManager.getCachedValue(this, new CachedAreasProvider());
+        return CachedValuesManager.getCachedValue(this,
+                () -> CachedValueProvider.Result.create(findAreasInner(), this));
     }
 
     @Nullable
@@ -112,196 +129,136 @@ public class ScriptPsiFile extends PsiFileBase {
         return mobFile != null ? (PsiMobObjectsBlock) mobFile.getChildren()[0] : null;
     }
 
-    private class CachedGlobalVarsProvider implements CachedValueProvider<List<EIGlobalVar>> {
-        @Nullable
-        @Override
-        public Result<List<EIGlobalVar>> compute() {
-            Stopwatch watch = Stopwatch.createStarted();
-            final EIGlobalVars globalVars = PsiTreeUtil.getChildOfType(ScriptPsiFile.this, EIGlobalVars.class);
-            List<EIGlobalVar> globalVarElements = PsiTreeUtil.getChildrenOfTypeAsList(globalVars, EIGlobalVar.class);
-            Result<List<EIGlobalVar>> result = new Result<>(
-                    globalVarElements,
-                    // TODO: useless?
-                    !globalVarElements.isEmpty() ? globalVarElements.toArray() : (globalVars != null ? globalVars : ScriptPsiFile.this)
-            );
-            LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " CachedGlobalVarsProvider");
-            return result;
-        }
-    }
-
-    private class CachedScriptDeclarationsProvider implements CachedValueProvider<List<EIScriptDeclaration>> {
-        @Nullable
-        @Override
-        public Result<List<EIScriptDeclaration>> compute() {
-            Stopwatch watch = Stopwatch.createStarted();
-            final EIDeclarations declarations = PsiTreeUtil.getChildOfType(ScriptPsiFile.this, EIDeclarations.class);
-            Result<List<EIScriptDeclaration>> result = new Result<>(
-                    declarations == null
-                            ? Collections.emptyList()
-                            : PsiTreeUtil.getChildrenOfTypeAsList(declarations, EIScriptDeclaration.class),
-                    declarations != null ? declarations : ScriptPsiFile.this
-            );
-            LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " CachedScriptDeclarationsProvider");
-            return result;
-        }
-    }
-
-    private class CachedScriptImplementationsProvider implements CachedValueProvider<List<EIScriptImplementation>> {
-        @Nullable
-        @Override
-        public Result<List<EIScriptImplementation>> compute() {
-            Stopwatch watch = Stopwatch.createStarted();
-            final EIScripts implementations = PsiTreeUtil.getChildOfType(ScriptPsiFile.this, EIScripts.class);
-            Result<List<EIScriptImplementation>> result = new Result<>(
-                    implementations == null
-                            ? Collections.emptyList()
-                            : PsiTreeUtil.getChildrenOfTypeAsList(implementations, EIScriptImplementation.class),
-                    implementations != null ? implementations : ScriptPsiFile.this
-            );
-            LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " CachedScriptImplementationsProvider");
-            return result;
-        }
-    }
-
-    private class CachedScriptLookupElementsProvider implements CachedValueProvider<List<LookupElement>> {
-        @Nullable
-        @Override
-        public Result<List<LookupElement>> compute() {
-            List<LookupElement> result = new ArrayList<>();
-            for (final EIScriptDeclaration script : findScriptDeclarations()) {
-                if (script.getName().length() > 0) {
-                    result.add(EILookupElementFactory.create(script));
+    @NotNull
+    // TODO: improve?
+    private Map<EITypeToken, List<LookupElement>> getGlobalVarLookupElementsInner() {
+        Map<EITypeToken, List<LookupElement>> result = new EnumMap<>(EITypeToken.class);
+        List<LookupElement> floatVars = new ArrayList<>();
+        List<LookupElement> stringVars = new ArrayList<>();
+        List<LookupElement> objectVars = new ArrayList<>();
+        List<LookupElement> groupVars = new ArrayList<>();
+        List<LookupElement> allVars = new ArrayList<>();
+        for (final EIGlobalVar globalVar : findGlobalVars()) {
+            if (globalVar.getName().length() > 0 && globalVar.getType() != null) {
+                LookupElement lookupElement = EILookupElementFactory.create(globalVar);
+                switch (globalVar.getType().getTypeToken()) {
+                    case VOID:
+                        break;
+                    case FLOAT:
+                        floatVars.add(lookupElement);
+                        break;
+                    case STRING:
+                        stringVars.add(lookupElement);
+                        break;
+                    case OBJECT:
+                        objectVars.add(lookupElement);
+                        break;
+                    case GROUP:
+                        groupVars.add(lookupElement);
+                        break;
                 }
             }
-            return new Result<>(result, ScriptPsiFile.this);
         }
+        allVars.addAll(floatVars);
+        allVars.addAll(stringVars);
+        allVars.addAll(objectVars);
+        allVars.addAll(groupVars);
+        result.put(EITypeToken.VOID, Collections.emptyList());
+        result.put(EITypeToken.FLOAT, Collections.unmodifiableList(floatVars));
+        result.put(EITypeToken.STRING, Collections.unmodifiableList(stringVars));
+        result.put(EITypeToken.OBJECT, Collections.unmodifiableList(objectVars));
+        result.put(EITypeToken.GROUP, Collections.unmodifiableList(groupVars));
+        result.put(EITypeToken.ANY, Collections.unmodifiableList(allVars));
+        return result;
     }
 
-    private class CachedGlobalVarLookupElementsProvider implements CachedValueProvider<Map<EITypeToken, List<LookupElement>>> {
-        @Nullable
-        @Override
-        public Result<Map<EITypeToken, List<LookupElement>>> compute() {
-            Map<EITypeToken, List<LookupElement>> result = new EnumMap<>(EITypeToken.class);
-            List<LookupElement> floatVars = new ArrayList<>();
-            List<LookupElement> stringVars = new ArrayList<>();
-            List<LookupElement> objectVars = new ArrayList<>();
-            List<LookupElement> groupVars = new ArrayList<>();
-            List<LookupElement> allVars = new ArrayList<>();
-            for (final EIGlobalVar globalVar : findGlobalVars()) {
-                if (globalVar.getName().length() > 0 && globalVar.getType() != null) {
-                    LookupElement lookupElement = EILookupElementFactory.create(globalVar);
-                    switch (globalVar.getType().getTypeToken()) {
-                        case VOID:
-                            break;
-                        case FLOAT:
-                            floatVars.add(lookupElement);
-                            break;
-                        case STRING:
-                            stringVars.add(lookupElement);
-                            break;
-                        case OBJECT:
-                            objectVars.add(lookupElement);
-                            break;
-                        case GROUP:
-                            groupVars.add(lookupElement);
-                            break;
-                    }
-                }
+    @NotNull
+    private List<LookupElement> getScriptLookupElementsInner() {
+        List<LookupElement> result = new ArrayList<>();
+        for (final EIScriptDeclaration script : findScriptDeclarations()) {
+            if (script.getName().length() > 0) {
+                result.add(EILookupElementFactory.create(script));
             }
-            allVars.addAll(floatVars);
-            allVars.addAll(stringVars);
-            allVars.addAll(objectVars);
-            allVars.addAll(groupVars);
-            result.put(EITypeToken.VOID, Collections.emptyList());
-            result.put(EITypeToken.FLOAT, Collections.unmodifiableList(floatVars));
-            result.put(EITypeToken.STRING, Collections.unmodifiableList(stringVars));
-            result.put(EITypeToken.OBJECT, Collections.unmodifiableList(objectVars));
-            result.put(EITypeToken.GROUP, Collections.unmodifiableList(groupVars));
-            result.put(EITypeToken.ANY, Collections.unmodifiableList(allVars));
-            return new Result<>(result, ScriptPsiFile.this);
         }
+        return result;
     }
 
     // TODO: implement with a HighlightingPass?
     // TODO: GSVars and Areas could be calculated in one PSI-tree pass, instead of two
-    private class CachedGSVarsProvider implements CachedValueProvider<Map<String, EIGSVar>> {
-        @Override
-        public Result<Map<String, EIGSVar>> compute() {
-            Stopwatch watch = Stopwatch.createStarted();
-            Map<String, EIGSVar> result = new LinkedHashMap<>(); // to preserve insertion order for structure view
-            acceptChildren(new EIVisitor() {
-                @Override
-                public void visitFunctionCall(@NotNull EIFunctionCall call) {
-                    super.visitFunctionCall(call);
+    @NotNull
+    private Map<String, EIGSVar> findGSVarsInner() {
+        Stopwatch watch = Stopwatch.createStarted();
+        Map<String, EIGSVar> result = new LinkedHashMap<>(); // to preserve insertion order for structure view
+        acceptChildren(new EIVisitor() {
+            @Override
+            public void visitFunctionCall(@NotNull EIFunctionCall call) {
+                super.visitFunctionCall(call);
 
-                    if (EIGSVar.isReadOrWrite(call)) {
-                        PsiElement nameElement = EIGSVar.getVarNameElement(call);
-                        if (nameElement != null &&
-                                // TODO: can also be an expression, how to handle then? a reference goes to a formal param
-                                //  for instance, instead of the actual parameter
-                                nameElement.getNode().getElementType() == ScriptTypes.CHARACTER_STRING) {
-                            String varName = EIGSVar.getVarName(nameElement.getText());
-                            EIGSVar stats = result.getOrDefault(varName, new EIGSVar(varName));
-                            stats.processCall(call);
-                            if (stats.isValid()) {
-                                result.put(varName, stats);
-                            }
+                if (EIGSVar.isReadOrWrite(call)) {
+                    PsiElement nameElement = EIGSVar.getVarNameElement(call);
+                    if (nameElement != null &&
+                            // TODO: can also be an expression, how to handle then? a reference goes to a formal param
+                            //  for instance, instead of the actual parameter
+                            nameElement.getNode().getElementType() == ScriptTypes.CHARACTER_STRING) {
+                        String varName = EIGSVar.getVarName(nameElement.getText());
+                        EIGSVar stats = result.getOrDefault(varName, new EIGSVar(varName));
+                        stats.processCall(call);
+                        if (stats.isValid()) {
+                            result.put(varName, stats);
                         }
                     }
                 }
+            }
 
-                @Override
-                public void visitElement(PsiElement element) {
-                    super.visitElement(element);
-                    if (!(element instanceof EIGlobalVars || element instanceof EIDeclarations)) {
-                        element.acceptChildren(this);
-                    }
-                }
-            });
-            LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " CachedGSVarsProvider");
-            return new Result<>(result, ScriptPsiFile.this);
-        }
-    }
-
-    private class CachedAreasProvider implements CachedValueProvider<Map<Integer, EIArea>> {
-        @Override
-        public Result<Map<Integer, EIArea>> compute() {
-            Stopwatch watch = Stopwatch.createStarted();
-            Map<Integer, EIArea> result = new LinkedHashMap<>(); // to preserve insertion order for structure view
-            acceptChildren(new EIVisitor() {
-                @Override
-                public void visitFunctionCall(@NotNull EIFunctionCall call) {
-                    super.visitFunctionCall(call);
-
-                    if (EIArea.isReadOrWrite(call)) {
-                        PsiElement areaIdElement = EIArea.getAreaIdElement(call);
-                        if (areaIdElement != null &&
-                                // TODO: same as with GSVars
-                                areaIdElement.getNode().getElementType() == ScriptTypes.FLOATNUMBER) {
-                            try {
-                                // TODO: also extract parsing to EIArea?
-                                int areaId = Integer.parseInt(areaIdElement.getText());
-                                EIArea stats = result.getOrDefault(areaId, new EIArea(areaId));
-                                stats.processCall(call);
-                                if (stats.isValid()) {
-                                    result.put(areaId, stats);
-                                }
-                            } catch (NumberFormatException ignored) {
-                                //don't really care
-                            }
-                        }
-                    }
-                }
-
-                @Override
-                public void visitElement(PsiElement element) {
-                    super.visitElement(element);
+            @Override
+            public void visitElement(PsiElement element) {
+                super.visitElement(element);
+                if (!(element instanceof EIGlobalVars || element instanceof EIDeclarations)) {
                     element.acceptChildren(this);
                 }
-            });
-            LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " CachedAreasProvider");
-            return new Result<>(result, ScriptPsiFile.this);
-        }
+            }
+        });
+        LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " findGSVarsInner");
+        return result;
+    }
+
+    @NotNull
+    private Map<Integer, EIArea> findAreasInner() {
+        Stopwatch watch = Stopwatch.createStarted();
+        Map<Integer, EIArea> result = new LinkedHashMap<>(); // to preserve insertion order for structure view
+        acceptChildren(new EIVisitor() {
+            @Override
+            public void visitFunctionCall(@NotNull EIFunctionCall call) {
+                super.visitFunctionCall(call);
+
+                if (EIArea.isReadOrWrite(call)) {
+                    PsiElement areaIdElement = EIArea.getAreaIdElement(call);
+                    if (areaIdElement != null &&
+                            // TODO: same as with GSVars
+                            areaIdElement.getNode().getElementType() == ScriptTypes.FLOATNUMBER) {
+                        try {
+                            // TODO: also extract parsing to EIArea?
+                            int areaId = Integer.parseInt(areaIdElement.getText());
+                            EIArea stats = result.getOrDefault(areaId, new EIArea(areaId));
+                            stats.processCall(call);
+                            if (stats.isValid()) {
+                                result.put(areaId, stats);
+                            }
+                        } catch (NumberFormatException ignored) {
+                            //don't really care
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void visitElement(PsiElement element) {
+                super.visitElement(element);
+                element.acceptChildren(this);
+            }
+        });
+        LOG.warn(watch.stop().elapsed(TimeUnit.MILLISECONDS) + " findAreasInner");
+        return result;
     }
 
     private class CachedCompanionPsiMobFileProvider implements CachedValueProvider<PsiMobFile> {
